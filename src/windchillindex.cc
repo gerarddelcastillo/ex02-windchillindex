94bcce899d846858be13deabd77a69be35c64ca5
gerardj.delcastillo@gmail.com <gerard_delcastillo1@my.vcccd.edu>
2018-12-14 11:01:47 -0800

    I thought I turned this in

--- src/windchillindex.cc
+++ src/windchillindex.cc
@@ -1,17 +1,13 @@
-#include <cmath>
+#include "math.h";
+#include <cfloat>
 
-namespace edu
-{
-	namespace vcccd
-	{
-		namespace vc
-		{
-			namespace csv13
-			{
-				double computeWindChillIndex(double temp, double vel)
-				{
-					if (temp > 10.0) return -1;
-					return 33.0 - ((10.0*sqrt(vel) - vel)*(33.0 - temp)) / 23.1;
+namespace edu {
+	namespace vcccd {
+		namespace vc {
+			namespace csv13 {
+				double computeWindChillIndex(double temperature, double windVelocity) {
+					if (temperature > 10.0) return FLT_MIN;
+					return 33.0 - ((10.0 * sqrt(windVelocity) - windVelocity) * (33.0 - temperature)) / 23.1;
 				}
 			}
 		}
